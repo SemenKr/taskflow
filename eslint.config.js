9dc5510ded8abcf36e437d8066551f033cc51d7b
semenkr <samkrekotyn@gmail.com>
2025-10-19 12:31:07 +0300

    setup project

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,27 @@
 import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import ts from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
 
-export default tseslint.config(
+export default [
   js.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...ts.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
     ignores: ['dist', 'node_modules'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      reactPlugin,
+    },
     rules: {
       '@typescript-eslint/no-unused-vars': 'error',
       '@typescript-eslint/no-explicit-any': 'warn',
+      'react/react-in-jsx-scope': 'off',
     },
-  }
-);
+  },
+];
