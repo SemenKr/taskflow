706ef345af04c9df36a51aa9069ad75cd4e83be0
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2025-10-19 18:19:49 +0300

    init modules scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { App } from './App.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/global.scss';
+import { App } from './App.tsx'; // 👈 подключаем глобальные стили
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
